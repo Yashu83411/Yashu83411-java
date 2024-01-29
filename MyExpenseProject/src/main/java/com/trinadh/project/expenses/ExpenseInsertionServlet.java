d1a5a43ad2a36e26663378b23df52963ceeb4879
Yashu83411 <147384154+Yashu83411@users.noreply.github.com>
2024-01-29 10:58:25 +0530

    Add files via upload

---
+++ MyExpenseProject/src/main/java/com/trinadh/project/expenses/ExpenseInsertionServlet.java
@@ -0,0 +1,55 @@
+package com.trinadh.project.expenses;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ExpenseInsertionServlet")
+public class ExpenseInsertionServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		DBConfiguration DB = new DBConfiguration();
+		
+		Connection con = DB.DBConfig();
+		
+		String sql = "insert into expenses (ExpenseDate, ExpenseCategory, ExpenseDetails, ExpenseAmount) values (?, ?, ?, ?)";
+			
+			//String userid = (String)req.getSession().getAttribute("userid");
+			String expdate = req.getParameter("ExpenseDate");
+			String expcat = req.getParameter("ExpenseCategory");
+			String expdetails = req.getParameter("ExpenseDetails");
+			String expamount = req.getParameter("ExpenseAmount");
+			
+
+			try {
+				PreparedStatement pst = con.prepareStatement(sql);
+				
+				pst.setString(1, expdate);
+				pst.setString(2, expcat);
+				pst.setString(3, expdetails);
+				pst.setString(4, expamount);
+				int rows = pst.executeUpdate();
+				
+				if (rows>0) {
+					System.out.println("Expense Insertion Successful.");
+					resp.sendRedirect("FetchUserExpenseServlet");
+				}
+				else {
+					System.out.println("Expense Insertion UnSuccessful.");
+					resp.sendRedirect("Homepage.html");
+				}
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			};
+	}
+}
---
+++ MyExpenseProject/src/main/java/com/trinadh/project/expenses/RegistrationServlet.java
@@ -0,0 +1,56 @@
+package com.trinadh.project.expenses;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		DBConfiguration DB = new DBConfiguration();
+		
+		Connection con = DB.DBConfig();
+		
+		String sql = "insert into login (UserName, UserPassword, ConfirmPassword) values (?, ?, ?)";
+		
+		try {
+			PreparedStatement pst = con.prepareStatement(sql);
+			String username = req.getParameter("UserName");
+			String password = req.getParameter("UserPassword");
+			String confirmpassword = req.getParameter("ConfirmPassword");
+			
+			pst.setString(1, username);
+			pst.setString(2, password);
+			pst.setString(3, confirmpassword);
+			int rows = pst.executeUpdate();
+			
+			if (rows>0) {
+				System.out.println("Registration Successful.");
+				resp.sendRedirect("User_Login.html");
+			}
+			else {
+				System.out.println("Registration UnSuccessful.");
+				resp.sendRedirect("User_Login.html");
+			}
+			
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+	}
+}
